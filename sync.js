7d8df8a91e0cd809a43342a98e4c7978c5208a5a
Rafael N <68340580+rfaaelnc@users.noreply.github.com>
2025-03-17 22:06:17 -0300

    Create sync.js

---
+++ sync.js
@@ -0,0 +1,41 @@
+const { Client } = require('@notionhq/client');
+const fs = require('fs');
+const path = require('path');
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+async function sync() {
+  try {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+    });
+
+    response.results.forEach(page => {
+      const title = page.properties.Name.title[0].plain_text;
+      const slug = page.properties.Slug.rich_text[0].plain_text;
+      const content = page.properties.Content.rich_text[0].plain_text;
+      const category = page.properties.Category.select.name;
+
+      const frontMatter = `---
+layout: page
+title: ${title}
+category: ${category}
+---
+
+${content}
+`;
+
+      fs.writeFileSync(
+        path.join(__dirname, `_docs/${slug}.md`),
+        frontMatter
+      );
+    });
+
+    console.log('Sincronização concluída!');
+  } catch (error) {
+    console.error('Erro:', error);
+  }
+}
+
+sync();
